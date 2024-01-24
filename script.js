a87a671538d934a86dfa29461ea66738fbfbdf70
Srayash <146334722+Srayash@users.noreply.github.com>
2024-01-25 00:38:54 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,18 @@
+function presentL1(){
+    document.getElementById('L1').style.backgroundColor="#4CAF4F";
+}
+function presentL2(){
+    document.getElementById('L2').style.backgroundColor="#4CAF4F";
+}
+function presentL3(){
+    document.getElementById('L3').style.backgroundColor="#4CAF4F";
+}
+function absentL1(){
+    document.getElementById('L1').style.backgroundColor="#F35B58";
+}
+function absentL2(){
+    document.getElementById('L2').style.backgroundColor="#F35B58";
+}
+function absentL3(){
+    document.getElementById('L3').style.backgroundColor="#F35B58";
+}
